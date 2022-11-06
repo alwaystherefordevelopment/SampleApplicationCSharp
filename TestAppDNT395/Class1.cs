d71f5f22be2ae431d973e0bbaa1d6c8c1747cf74
Kumar Abhishek <kumarabhishek.1109@live.com>
2022-11-06 16:14:47 +0530

    Add project files.

---
+++ TestAppDNT395/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppDNT395
+{
+    class Class1
+    {
+        //This is a single line comment
+        /*static void Main()
+        {
+            Console.WriteLine("Hello From Class1");
+        }*/
+        int i = 10;
+    }
+}
